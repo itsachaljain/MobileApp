d34644432e1ee4ed3e89628ee170c651e4fc1df7
itsachaljain <achaljain785@gmail.com>
2020-10-03 01:12:58 +0530

    Assigment 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,11 +1,47 @@
 import React, { Component } from "react";
+import Home from "./HomeComponent";
 import Menu from "./MenuComponent";
+import About from "./AboutComponent";
+import Contact from "./ContactComponent";
 import Dishdetail from "./DishdetailComponent";
-import { View, Platform } from "react-native";
+import { View, Platform, StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
+const HomeNavigator = createStackNavigator();
+const AboutNavigator = createStackNavigator();
 const MenuNavigator = createStackNavigator();
+const ContactNavigator = createStackNavigator();
+
+const MainNavigator = createDrawerNavigator();
+
+function MainNavigatorDrawer() {
+	return (
+		<MainNavigator.Navigator drawerStyle={{ backgroundColor: "#D1C4E9" }}>
+			<MainNavigator.Screen
+				name="Home"
+				component={HomeNavigatorScreen}
+				options={{ title: "Home" }}
+			/>
+			<MainNavigator.Screen
+				name="About"
+				component={AboutNavigatorScreen}
+				options={{ title: "About Us" }}
+			/>
+			<MainNavigator.Screen
+				name="Menu"
+				component={MenuNavigatorScreen}
+				options={{ title: "Menu" }}
+			/>
+			<MainNavigator.Screen
+				name="Contact"
+				component={ContactNavigatorScreen}
+				options={{ title: "Contact Us" }}
+			/>
+		</MainNavigator.Navigator>
+	);
+}
 
 function MenuNavigatorScreen() {
 	return (
@@ -31,12 +67,76 @@ function MenuNavigatorScreen() {
 	);
 }
 
+function HomeNavigatorScreen() {
+	return (
+		<HomeNavigator.Navigator
+			initialRouteName="Home"
+			screenOptions={{
+				headerStyle: {
+					backgroundColor: "#512DA8",
+				},
+				headerTintColor: "#fff",
+				headerTitleStyle: {
+					color: "#fff",
+				},
+			}}
+		>
+			<HomeNavigator.Screen name="Home" component={Home} />
+		</HomeNavigator.Navigator>
+	);
+}
+
+function AboutNavigatorScreen() {
+	return (
+		<AboutNavigator.Navigator
+			initialRouteName="About"
+			screenOptions={{
+				headerStyle: {
+					backgroundColor: "#512DA8",
+				},
+				headerTintColor: "#fff",
+				headerTitleStyle: {
+					color: "#fff",
+				},
+			}}
+		>
+			<AboutNavigator.Screen name="About Us" component={About} />
+		</AboutNavigator.Navigator>
+	);
+}
+
+function ContactNavigatorScreen() {
+	return (
+		<ContactNavigator.Navigator
+			initialRouteName="Contact"
+			screenOptions={{
+				headerStyle: {
+					backgroundColor: "#512DA8",
+				},
+				headerTintColor: "#fff",
+				headerTitleStyle: {
+					color: "#fff",
+				},
+			}}
+		>
+			<ContactNavigator.Screen name="Contact Us" component={Contact} />
+		</ContactNavigator.Navigator>
+	);
+}
+
 class Main extends Component {
 	render() {
 		return (
-			<NavigationContainer>
-				<MenuNavigatorScreen />
-			</NavigationContainer>
+			<View
+				style={{
+					flex: 1,
+					paddingTop: Platform.OS === "android" ? 0 : StatusBar.currentHeight,
+				}}
+			>
+				<NavigationContainer>
+					<MainNavigatorDrawer />
+				</NavigationContainer>
+			</View>
 		);
 	}
 }
