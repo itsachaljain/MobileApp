128bab8c13032e68cd4bf6e1c1cf7ab772282fb1
itsachaljain <achaljain785@gmail.com>
2020-10-05 22:31:55 +0530

    Modals

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -6,6 +6,7 @@ import {
 	StyleSheet,
 	Switch,
 	Button,
+	Modal,
 } from "react-native";
 import { Picker } from "@react-native-community/picker";
 import DatePicker from "react-native-datepicker";
@@ -17,16 +18,25 @@ class Reservation extends Component {
 			guests: 1,
 			smoking: false,
 			date: "",
+			showModal: false,
 		};
 	}
 
-	handleReservation() {
-		console.log(JSON.stringify(this.state));
+	toggleModal() {
+		this.setState({ showModal: !this.state.showModal });
+	}
+
+	resetForm() {
 		this.setState({
 			guests: 1,
 			smoking: false,
 			date: "",
 		});
+	}
+
+	handleReservation() {
+		console.log(JSON.stringify(this.state));
+		this.toggleModal();
 	}
 
 	render() {
@@ -93,6 +103,40 @@ class Reservation extends Component {
 						accessibilityLabel="Learn more about this purple button"
 					/>
 				</View>
+				<Modal
+					animationType={"slide"}
+					transparent={false}
+					visible={this.state.showModal}
+					onDismiss={() => {
+						this.toggleModal();
+						this.resetForm();
+					}}
+					onRequestClose={() => {
+						this.toggleModal();
+						this.resetForm();
+					}}
+				>
+					<View style={styles.modal}>
+						<Text style={styles.modalTitle}>Your Reservation</Text>
+						<Text style={styles.modalText}>
+							Number of Guests: {this.state.guests}
+						</Text>
+						<Text style={styles.modalText}>
+							Smoking? : {this.state.smoking ? "Yes" : "No"}
+						</Text>
+						<Text style={styles.modalText}>
+							Date and Time: {this.state.date}
+						</Text>
+						<Button
+							onPress={() => {
+								this.toggleModal();
+								this.resetForm();
+							}}
+							color="#512DA8"
+							title="Close"
+						/>
+					</View>
+				</Modal>
 			</ScrollView>
 		);
 	}
@@ -112,6 +156,22 @@ const styles = StyleSheet.create({
 	},
 	formItem: {
 		flex: 1,
+	},
+	modal: {
+		justifyContent: "center",
+		margin: 20,
+	},
+	modalTitle: {
+		fontSize: 24,
+		fontWeight: "bold",
+		backgroundColor: "#512DA8",
+		textAlign: "center",
+		color: "white",
+		marginBottom: 20,
+	},
+	modalText: {
+		fontSize: 18,
+		margin: 10,
 	},
 });
 
